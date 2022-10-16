c62784093f8e7e182d7705b195e9f2f493b42e31
Command17 <84639718+Command17@users.noreply.github.com>
2022-10-16 18:35:26 +0200

    Create Plugin.lua

---
+++ src/Plugin.lua
@@ -0,0 +1,54 @@
+local HttpService = game:GetService("HttpService")
+
+local Plugin = script.Parent
+local Packages = Plugin.Packages
+
+local PluginBar = plugin:CreateToolbar("CMD by baum")
+local PluginButton = PluginBar:CreateButton("CMD_Button", "Open CMD", "http://www.roblox.com/asset/?id=11295128691", "CMD")
+
+local WidgetInfo = DockWidgetPluginGuiInfo.new(Enum.InitialDockState.Float, false, false, 500, 250, 250, 125)
+local Widget = plugin:CreateDockWidgetPluginGui("CMDWidget", WidgetInfo)
+
+local Events = Plugin.Events
+local CMD = require(Plugin.CMD)
+
+local PackagesToSave = {}
+local EXTRA_PACKAGES_KEY = "cmd_extra_packages"
+
+Widget.Title = "CMD"
+
+CMD:loadPackages()
+CMD:createInterface(Widget)
+
+Widget:GetPropertyChangedSignal("Enabled"):Connect(function()
+	PluginButton:SetActive(Widget.Enabled)
+end)
+
+PluginButton.Click:Connect(function()
+	Widget.Enabled = not Widget.Enabled
+end)
+
+Events.PackageAdded.Event:Connect(function(Package)
+	PackagesToSave[Package.Name] = Package.Source
+	
+	plugin:SetSetting(EXTRA_PACKAGES_KEY, HttpService:JSONEncode(PackagesToSave))
+end)
+
+Events.PackageRemoved.Event:Connect(function(Package)
+	PackagesToSave[Package] = nil
+end)
+
+local succes, err = pcall(function()
+	PackagesToSave = HttpService:JSONDecode(plugin:GetSetting(EXTRA_PACKAGES_KEY))
+
+	for i, PackageSource in pairs(PackagesToSave) do
+		if PackageSource ~= nil then
+			local Package = Instance.new("ModuleScript")
+			Package.Name = i
+			Package.Source = PackageSource
+			Package.Parent = Packages
+
+			CMD:addPackage(Package)
+		end
+	end
+end)
