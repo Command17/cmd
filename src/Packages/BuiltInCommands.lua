cfce04f15dd509d60c6dc9f351498d91b548a34e
Command17 <84639718+Command17@users.noreply.github.com>
2022-10-18 15:46:55 +0200

    Update BuiltInCommands.lua

--- src/Packages/BuiltInCommands.lua
+++ src/Packages/BuiltInCommands.lua
@@ -6,6 +6,8 @@ local NonDeletablePackages = {
 
 local Fonts = {}
 
+local BackgroundColors = {}
+
 for i, v in ipairs(Enum.Font:GetEnumItems()) do
 	local font = string.split(tostring(v), ".")[3]
 	
@@ -29,12 +31,12 @@ return {
 		end
 	end,
 	
-	["font"] = {
-		["get"] = function(args, cmdInterface, cmd)
+	["settings"] = {
+		["getFont"] = function(args, cmdInterface, cmd)
 			cmdInterface:newMsg("The current font is: " .. string.split(tostring(cmdInterface:getFont()), ".")[3])
 		end,
 		
-		["set"] = function(args, cmdInterface, cmd)
+		["setFont"] = function(args, cmdInterface, cmd)
 			local font = args[1]
 			
 			if font then
@@ -54,6 +56,62 @@ return {
 			for i, v in pairs(Fonts) do
 				cmdInterface:newMsg(i, Color3.fromRGB(0, 255, 0))
 			end
+		end,
+		
+		["getBackgroundColor"] = function(args, cmdInterface, cmd)
+			local color = cmdInterface:getBackgroundColor()
+			
+			cmdInterface:newMsg(string.format("The current background color is: %s, %s, %s", tostring(math.round(color.R * 255)), tostring(math.round(color.G * 255)), tostring(math.round(color.B * 255))))
+		end,
+		
+		["setBackgroundColor"] = function(args, cmdInterface, cmd)
+			local color1 = args[1]
+			local color2 = args[2]
+			local color3 = args[3]
+			
+			if color1 then
+				if color1 == "Studio" then
+					cmdInterface:useStudioBackgroundColor()
+				elseif color1 and color2 and color3 then
+					local c1 = tonumber(color1)
+					local c2 = tonumber(color2)
+					local c3 = tonumber(color3)
+					
+					if c1 and c2 and c3 then
+						local Color = Color3.fromRGB(c1, c2, c3)
+						
+						cmdInterface:setBackgroundColor(Color)
+						
+						cmdInterface:newMsg(string.format("Set background color to: %s, %s, %s", c1, c2, c3))
+					else
+						cmdInterface:newMsg("Argument(s) are wrong")
+					end
+				else
+					cmdInterface:newMsg("Argument(s) are missing!")
+				end
+			else
+				cmdInterface:newMsg("Argument(s) are missing!")
+			end
+		end,
+	},
+	
+	["version"] = {
+		["latest"] = function(args, cmdInterface, cmd)
+			local latest = nil
+			
+			local succes, err = pcall(function()
+				latest = HttpService:GetAsync("https://raw.githubusercontent.com/Command17/cmd/main/version.txt")
+			end)
+			
+			if succes and latest then
+				cmdInterface:newMsg("Latest version is: " .. latest)
+			else
+				cmdInterface:newMsg("Could not get latest version")
+			end
+		end,
+		
+		["current"] = function(args, cmdInterface, cmd)
+			cmdInterface:newMsg("Current version: " .. cmd.Version)
 		end,
 	},
 	
